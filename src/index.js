da950e5bbfad54ae7cdaca20cd83173c6eb40d5b
SgAlone <mtung280196@gmail.com>
2020-10-11 23:56:46 +0700

    view translate

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ import "./assets/css/animate.min.css";
 import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
 import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
+import "./assets/css/translate.css";
 
 import AdminLayout from "layouts/Admin.jsx";
 import Translate from "./layouts/Translate";
--- src/layouts/Translate.jsx
+++ src/layouts/Translate.jsx
@@ -1,108 +1,97 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, {Component} from "react";
-import axios from "axios";
+import axios from "axios"
+import renderHTML from 'react-render-html';
 
 class Translate extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            word: '',
-            pronunciation: '',
-            syllables: [],
-            definition: []
+            definition: [],
+            word_net: [],
+            sentence: [],
+            synonyms: [],
+            rel_word: null
         };
     }
 
     componentDidMount() {
         const search = this.props.match.params.translate;
-        axios.get("http://localhost:8080/search?search=" + search)
+        axios.get("http://localhost:8080/translate/" + search)
             .then((result) => {
                     const data = result.data;
                     this.setState({
-                        word: data.word,
-                        pronunciation: data.pronunciation,
-                        syllables: data.syllables,
-                        definition: data.results
+                        definition: data.definition,
+                        word_net: data.word_net,
+                        sentence: data.sentence,
+                        synonyms: data.synonyms,
+                        rel_word: data.rel_word
                     })
                 }
             )
     }
 
-    renderEachDefinition(definition, index) {
-        if (!definition) {
-            return (<div key={index}></div>)
-        }
-        return (
-            <div key={index}>
-                <hr/>
-                <p>{definition.partOfSpeech}</p>
-                <p>{definition.definition}</p>
-                <p>
-                    {definition.synonyms && definition.synonyms.map(function (d, idx) {
-                        return (<li key={idx}>{d}</li>)
-                    })}
-                </p>
-
-                <p>
-                    {definition.typeOf && definition.typeOf.map(function (d, idx) {
-                        return (<li key={idx}>{d}</li>)
-                    })}
-                </p>
-
-                <p>
-                    {definition.examples && definition.examples.map(function (d, idx) {
-                        return (<li key={idx}>{d}</li>)
-                    })}
-                </p>
-            </div>
-        )
-    }
-
     render() {
-        let syllables = '';
-        let definitiones = [];
-        if (this.state.syllables && this.state.syllables.length > 0) {
-            this.state.syllables[0].list.forEach(item => {
-                syllables += item + ".";
-            })
-        }
-        if (this.state.definition && this.state.definition.length > 0) {
-            this.state.definition.forEach((item, index) => {
-                definitiones.push(this.renderEachDefinition(item, index))
-            })
-        }
         return (
             <div className="content">
-                <Grid fluid>
-                    <div>
-                        <span><b>Dictionary:</b></span>&nbsp;
-                        <span>{this.state.word}</span>&nbsp;&nbsp;
-                        (<span>{this.state.pronunciation.all}</span>)
-                    </div>
+                <div className="definition new-line">
+                    <h5>Định nghĩa: </h5>
+                    {this.state.definition.map(item => {
+                        return item + ' \n ';
+                    })}
+                </div>
+                <hr/>
+                {/*<div className="word-net">*/}
+                {/*    {this.state.word_net.map((item, index) => {*/}
+                {/*        return (<div key={index}>*/}
+                {/*            <span>{item.pos}</span> <br/>*/}
+                {/*            {item.tr.map((tr, keyTr) => {*/}
+                {/*                return (<div key={keyTr}>*/}
+                {/*                    <span>{keyTr}. {tr.l}</span><br/>*/}
+                {/*                    {tr.similar && tr.similar.length > 0 &&*/}
+                {/*                    <span>Synonyms: <span>{tr.similar.map(s => s + '/')}</span></span>*/}
+                {/*                    }*/}
+                {/*                    <br/>*/}
+                {/*                </div>)*/}
+                {/*            })}*/}
+                {/*        </div>)*/}
+                {/*    })}*/}
+                {/*</div>*/}
+                {/*<hr/>*/}
 
-                    <div>
-                        Syllables: {syllables}
+                <div className="sample-sentences">
+                    <h5>Vi du:</h5>
+                    {this.state.sentence.map((item, index) => {
+                        return (<div key={index}>
+                            <span>{index + 1}. {renderHTML(item.foreign)} </span><br/><br/>
+                        </div>)
+                    })}
+                </div>
+                <hr/>
+
+                <div className="synosyms">
+                    {this.state.synonyms && this.state.synonyms.map((item, index) => {
+                        return (<div key={index}>
+                            <span>{item.pos}{item.tran}</span><br/>
+                            {item.ws.map(ws => " " + ws.w + " / ")}
+                            <br/><br/>
+                        </div>)
+                    })}
+                    <hr />
+                </div>
+
+                {this.state.rel_word &&
+                    <div className="rel-word">
+                        <span>Lien hop: {this.state.rel_word.word} </span> <br/><br/>
+                        {this.state.rel_word.rels.map((item, index) => {
+                            return (<div key={index}>
+                                <span>{item.rel.pos}</span><br/>
+                                {item.rel.words.map(w => w.word + " / ")}
+                                <br/><br/>
+                            </div>)
+                        })}
                     </div>
-                    <div>
-                        {definitiones}
-                    </div>
-                </Grid>
+                }
+
             </div>
         );
     }
